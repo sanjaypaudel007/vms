08fffb1725d0446448aa47b958cc77522be4a962
samir shrestha <sam.samirshrestha@gmail.com>
2016-01-01 15:08:09 -0600

    staff added

--- src/main/java/com/practice/vms/entity/Staff.java
+++ src/main/java/com/practice/vms/entity/Staff.java
@@ -1,5 +1,58 @@
 package com.practice.vms.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.Valid;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
 public class Staff {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	@NotEmpty 
+	private String name;
+	@NotEmpty 
+	private String post;
+	@NotEmpty 
+	private Address address;
+	@NotEmpty 
+	private Long phone;
+	@Valid
+	private String Department;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPost() {
+		return post;
+	}
+	public void setPost(String post) {
+		this.post = post;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public Long getPhone() {
+		return phone;
+	}
+	public void setPhone(Long phone) {
+		this.phone = phone;
+	}
+	public String getDepartment() {
+		return Department;
+	}
+	public void setDepartment(String department) {
+		Department = department;
+	}
+	
 
 }
